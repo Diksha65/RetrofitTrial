021145993dffdcf7673ed725305f37c4ab3faaeb
shaggy089 <alishahroz089@gmail.com>
2017-03-17 17:59:19 +0530

    sshopiupgfytdhg

--- app/src/main/java/com/example/diksha/retrofit/FriendsModel.java
+++ app/src/main/java/com/example/diksha/retrofit/FriendsModel.java
@@ -4,9 +4,13 @@ package com.example.diksha.retrofit;
  * Created by diksha on 15/3/17.
  */
 
+import android.content.Context;
+
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -14,14 +18,28 @@ public class FriendsModel {
 
     @SerializedName("friends")
     @Expose
-    private ArrayList<Friend> friends = null;
+    private List<Friend> friends = null;
 
-    public ArrayList<Friend> getFriends() {
+    public List<Friend> getfriends() {
         return friends;
     }
 
-    public void setFriends(ArrayList<Friend> friends) {
+    public void setFriends(List<Friend> friends) {
         this.friends = friends;
+    }
+
+    /**
+     * @param context Todo What is this for?
+     *                Changed private to public.
+     */
+    public FriendsModel(Context context){
+        friends = new ArrayList<>();
+        for(int i=0;i<100;i++){
+            Friend friend = new Friend();
+            friend.setName("Friend #" + i);
+            friend.setRollNo("1000" + i);
+            friend.setBranch("Branch %" + i);
+        }
     }
 
 }
--- app/src/main/java/com/example/diksha/retrofit/MainActivity.java
+++ app/src/main/java/com/example/diksha/retrofit/MainActivity.java
@@ -5,9 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
+    private CardAdapter mAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +24,56 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         mRecyclerView.setLayoutManager(layoutManager);
+        updateUI();
     }
+    private void updateUI(){
+        //Todo not sure if this is correct.
+        FriendsModel friendsModel= new FriendsModel(getApplicationContext());
+        List<Friend> friends = friendsModel.getfriends();
+        mAdapter = new CardAdapter(friends);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public class CardHolder extends RecyclerView.ViewHolder{
+        public TextView mname;
+        public TextView mroll_no;
+        public TextView mBranch;
+
+        public CardHolder(View v){
+            super(v);
+            mname = (TextView)v.findViewById(R.id.text_view_name);
+            mroll_no = (TextView)v.findViewById(R.id.text_view_roll);
+            mBranch = (TextView)v.findViewById(R.id.text_view_branch);
+
+        }
+    }
+
+    public class CardAdapter extends RecyclerView.Adapter<CardHolder>{
+        private List<Friend> mFriends;
+        public CardAdapter(List<Friend> friend){
+            mFriends = friend;
+        }
+        @Override
+        public CardHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main, parent, false);
+            return new CardHolder(v);
+        }
+
+
+        @Override
+        public void onBindViewHolder(CardHolder holder, int position) {
+                   Friend friend = mFriends.get(position);
+            holder.mname.setText(friend.getName());
+            holder.mroll_no.setText(friend.getRollNo());
+            holder.mBranch.setText(friend.getBranch());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mFriends.size();
+        }
+    }
+
+
 }
+
